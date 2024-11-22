77360486faae90c55385ca450881ff681b2ee1cc
Pinguicidgon <144015906+Pinguicidgon@users.noreply.github.com>
2024-11-22 12:20:24 +0100

    types.ts

--- types.ts
+++ types.ts
@@ -1,18 +1,20 @@
-import { ObjectId } from "mongodb";
+import { ObjectId } from "mongodb"; // Importa ObjectId desde mongodb para usar como tipo de identificador único
 
+// Modelo de datos para un niño
 export type KidModel = {
   _id?: ObjectId;
   nombre: string;
-  comportamiento: "bueno" | "malo";
-  ubicacion: ObjectId; // Referencia a la colección "ubicacion"
+  comportamiento: "bueno" | "malo"; // Estado de comportamiento, puede ser "bueno" o "malo"
+  ubicacion: ObjectId; // Referencia al ObjectId del lugar asociado
 };
 
+// Modelo de datos para un lugar
 export type LugarModel = {
   _id?: ObjectId;
   nombre: string;
-  coordenadas: {
-    lat: number; // Latitude
-    long: number; // Longitude
+  coordenadas: { // Coordenadas del lugar
+    lat: number; // Latitud
+    long: number; // Longitud
   };
-  numeroNinosBuenos: number; // Contador de niños buenos
+  numeroNinosBuenos: number; // Numero de niños buenos en ese lugar
 };
